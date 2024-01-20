8175494f0e341d3eefeb7787277afae1e9e46c75
AlirezaAghaee1996 <alireza.aghaee70@gmail.com>
2024-01-20 15:01:16 +0330

    update route

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
-import userRouter from './routes/userRoute'
-import productRouter from './routes/productRoute'
-import categoryRouter from './routes/categoryRoute'
-import commentRouter from './routes/commentRoute'
-import orderRouter from './routes/orderRoute'
-import searchRouter from './routes/searchRoute'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
+import categoryRouter from './routes/categoryRoute.js'
+import commentRouter from './routes/commentRoute.js'
+import orderRouter from './routes/orderRoute.js'
+import searchRouter from './routes/searchRoute.js'
+import HandleERROR from './utils/handleError.js'
+import { catchError } from './utils/catchError.js'
 const app=express()
 app.use(morgan('dev'))
 app.use(express.json())
@@ -18,5 +20,9 @@ app.use('/api/v1/category',categoryRouter)
 app.use('/api/v1/comment',commentRouter)
 app.use('/api/v1/order',orderRouter)
 app.use('/api/v1/search',searchRouter)
+app.use('*',(req,res,next)=>{
+    next(new HandleERROR(`can't find ${req.originalUrl} url on server`,404))
+})
+app.use(catchError)
 
 export default app
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,8 @@
 import  express  from "express";
+import { deleteUser, getAllUsers, login, register, updateProfile } from "../controllers/userCn.js";
 const userRouter=express.Router()
-
-
+userRouter.route('/').get(getAllUsers).post(login)
+userRouter.route('/:id').delete(deleteUser).patch(updateProfile)
+userRouter.route('/register').post(register)
 
 export default userRouter
